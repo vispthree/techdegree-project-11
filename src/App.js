93806bf3753850f56c1e4ed9b7474106ddbaccd0
AJ <alainkassabian@gmail.com>
2018-08-27 20:16:02 -0400

    Fetch images from flickr api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import apiKey from './config';
 class App extends Component {
-  render() {
+
+  constructor() {
+    super();
+    this.state = {
+      galleryItems: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=trees&format=json`)
+      .then((response) => {
+        this.setState({ galleryItems: response.data });
+        console.log(this.state.galleryItems);      
+     })
+     .catch(error => {
+       console.log('Error fetching and parsing flickr data', error);
+     });
+  }
+
+  render() {    
     return (
       <div className="App">
         <header className="App-header">
