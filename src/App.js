686454d887fa21b07c92f92f54a1fe1e83bd9c1d
AJ <alainkassabian@gmail.com>
2018-08-30 09:16:44 -0400

    Created urls from flickr api data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import apiKey from './config';
+
+const GalleryItemHTML = props => (
+  <li className="gallery-item-wrap">
+    <img src={props.url} alt=""/>
+  </li>
+);
+
+const GalleryItemList = props => {
+
+  const results = props.data;
+  let images = results.map(image =>
+    <GalleryItemHTML url={`https://www.flickr.com/photos${image.owner}/${image.id}`} />
+  );
+
+  return(
+    <ul className="gallery-item-list">
+      {GalleryItemHTML}
+    </ul>
+  );
+}
+
 class App extends Component {
 
   constructor() {
@@ -13,10 +34,12 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=trees&format=json`)
-      .then((response) => {
-        this.setState({ galleryItems: response.data });
-        console.log(this.state.galleryItems);      
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=trees&format=json&nojsoncallback=1`)
+      .then(response => {         
+        this.setState({
+          galleryItems: response.data.photos
+        })
+        console.log(`first title: ${this.state.photo[0].title}`);
      })
      .catch(error => {
        console.log('Error fetching and parsing flickr data', error);
@@ -30,9 +53,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+          <GalleryItemList data={this.state.galleryItems} />
+
       </div>
     );
   }
