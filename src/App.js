cc9575a354d27e22b68db3359002590944dad8c5
AJ <alainkassabian@gmail.com>
2018-09-12 08:30:50 -0400

    Search function reroutes and siplays images

--- src/App.js
+++ src/App.js
@@ -3,59 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import apiKey from './config';
+import SearchForm from './SearchForm';
 import {
   BrowserRouter,
   Route,
   NavLink,
-  Redirect
 } from 'react-router-dom';
-
-
-class SearchForm extends React.Component {
-
-  state = {
-    searchText: ''
-  }
-
-  onSearchChange = e => {
-    this.setState({ searchText: e.target.value });
-    console.log('search changed');
-  }
-
-  onSubmit = e => {
-    e.preventDefault();
-    /*<Dogs query={this.state.searchText} /> */    
-
-    this.performSearch(this.state.searchText);
-
-    
-
-    e.currentTarget.reset();
-  }
-
-  performSearch(query){
-
-    console.log(`search term rendered: ${query}`);
-    this.props.sendData(query);
-    this.props.history.push('/seach');  
-
-  }
-
-  render() {
-      return(
-        <form className="search--form" onSubmit={this.onSubmit} >
-          <input type="search"
-            onChange={this.onSearchChange}
-            name="search"
-            ref={(input) => this.query = input}
-            placeholder="Search" />
-          <button type="submit" id="submit" className="search--button">O</button>        
-        </form>
-      );
-  }
-
-}
-
 
 const Navbar = props => (
   <ul>
@@ -67,12 +20,7 @@ const Navbar = props => (
 
 class Container extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      galleryItems: []
-    };
-  }
+  state = {galleryItems: []}
 
   componentDidMount() {
     axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${this.props.query}&format=json&nojsoncallback=1`)
@@ -160,9 +108,21 @@ const GalleryItemList = props => {
 
 class App extends Component {
 
-    getData(query){
-      console.log(`Search at top of app: ${query}`);
+
+  constructor() {
+    super();
+    this.state = {
       
+      searchQuery: 'cats'
+    };
+  }
+
+
+    getData = query => {
+      console.log(`Search at top of app: ${query}`);
+      this.setState({ searchQuery: query }, function(){
+        console.log(`query state: ${this.state.searchQuery}`);
+      });      
     }
 
     render() {    
@@ -180,7 +140,7 @@ class App extends Component {
             <Route path="/forests" render={ () => <Forests query="forests" />} /> {/*Pass search term with nav select*/}
             <Route path="/waterfalls" render={ () => <Waterfalls query="waterfalls" />} />
             <Route path="/dogs" render={ () => <Dogs query="shepherd dog" />} />
-            <Route path="/search" render={ () => <Search query={'balloon'} />} />
+            <Route path="/search" render={ () => <Search query={this.state.searchQuery} />} />
 
           </div>
         </BrowserRouter>
---
+++ src/SearchForm.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {
+  withRouter,
+} from 'react-router-dom';
+
+class SearchForm extends React.Component {
+
+  state = {
+    searchText: ''
+  }
+
+  onSearchChange = e => {
+    this.setState({ searchText: e.target.value });
+    console.log('search changed');
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+    /*<Dogs query={this.state.searchText} /> */    
+
+    this.performSearch(this.state.searchText);
+
+    e.currentTarget.reset();
+  }
+
+  performSearch(query){
+
+    console.log(`search term rendered: ${query}`);
+    this.props.sendData(query);
+
+    this.props.history.push('/search');
+    Search.forceUpdate();   
+    
+  }
+
+  render() {
+      return(
+        <form className="search--form" onSubmit={this.onSubmit} >
+          <input type="search"
+            onChange={this.onSearchChange}
+            name="search"
+            ref={(input) => this.query = input}
+            placeholder="Search" />
+          <button type="submit" id="submit" className="search--button">O</button>        
+        </form>
+      );
+  }
+
+}
+
+export default withRouter(SearchForm);
